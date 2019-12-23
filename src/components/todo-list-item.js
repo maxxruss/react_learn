4226fc023d20340ad772f7718ab7bc57060257ee
Игнатьев Максим Юрьевич <ignatev.my@kry.local>
2019-12-23 09:21:36 +0700

    кнопки у списка

--- src/components/todo-list-item.js
+++ src/components/todo-list-item.js
@@ -1,13 +1,50 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Button from "@material-ui/core/Button";
+import DeleteOutlineOutlinedIcon from "@material-ui/icons/DeleteOutlineOutlined";
+import PriorityHighOutlinedIcon from "@material-ui/icons/PriorityHighOutlined";
 
-const ToDoListItem = ({label, important=false}) => {
+const useStyles = makeStyles(theme => ({
+  margin: {
+    margin: theme.spacing(1)
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const ToDoListItem = ({ id, label, important = false }) => {
+  const classes = useStyles();
 
   const style = {
-    color: important ? 'tomato' : 'black'
-  }
-    return (
-    <span style = {style}>{label}</span>
-    );
+    color: important ? "tomato" : "black"
   };
+  return (
+    <ListItem key={id} button dense>
+      <ListItemText style={style} primary={label} />
+      <ListItemSecondaryAction>
+        <Button
+          variant="outlined"
+          size="small"
+          color="primary"
+          className={classes.margin}
+        >
+          <DeleteOutlineOutlinedIcon />
+        </Button>
+        <Button
+          variant="outlined"
+          size="small"
+          color="secondary"
+          className={classes.margin}
+        >
+          <PriorityHighOutlinedIcon />
+        </Button>
+      </ListItemSecondaryAction>
+    </ListItem>
+  );
+};
 
-  export default ToDoListItem
+export default ToDoListItem;
--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -1,31 +1,14 @@
 import React from "react";
-// import ToDoListItem from "./todo-list-item";
+import ToDoListItem from "./todo-list-item";
 import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import ListItemText from "@material-ui/core/ListItemText";
-import IconButton from "@material-ui/core/IconButton";
-import DeleteIcon from "@material-ui/icons/Delete";
 import "./todo-list.css";
 
 const ToDoList = ({ todos }) => {
   const [dense] = React.useState(false);
-  // const [secondary] = React.useState(false);
-
   const elements = todos.map(item => {
     const { id, label } = item;
     return (
-      <ListItem key={id} button dense>
-        <ListItemText
-          primary={label}
-          // secondary={secondary ? "Secondary text" : null}
-        />
-        <ListItemSecondaryAction>
-          <IconButton edge="end" aria-label="delete">
-            <DeleteIcon />
-          </IconButton>
-        </ListItemSecondaryAction>
-      </ListItem>
+     <ToDoListItem id={id} label={label}/>
     );
   });
   return <List dense={dense}>{elements}</List>;
